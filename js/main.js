2b53105f82b12ea82324ee3564a23b1d869549e8
Наталушко Андрій Вікторович <natalushko.a@agromat.intranet>
2021-10-06 23:06:32 +0300

    структура папок

---
+++ js/main.js
@@ -0,0 +1,8 @@
+import refs from './refs.js';
+const { daysT, hoursT, minsT, secsT } = refs;
+//console.log(daysT);
+
+// const days = Math.floor(time / (1000 * 60 * 60 * 24));
+// const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+// const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
+// const secs = Math.floor((time % (1000 * 60)) / 1000);

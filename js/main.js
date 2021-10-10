53363d9e56dc064a6cd8c4c205f859bf02fedfed
Наталушко Андрій Вікторович <natalushko.a@agromat.intranet>
2021-10-10 14:16:40 +0300

    Таймер обратного отсчета

--- js/main.js
+++ js/main.js
@@ -1,8 +1,50 @@
-import refs from './refs.js';
-const { daysT, hoursT, minsT, secsT } = refs;
-//console.log(daysT);
+import refs from '../js/refs.js';
+const { daysTimer, hoursTimer, minsTimer, secsTimer } = refs;
 
-// const days = Math.floor(time / (1000 * 60 * 60 * 24));
-// const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-// const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
-// const secs = Math.floor((time % (1000 * 60)) / 1000);
+class CountdownTimer {
+  constructor(targetDate, markup) {
+    this.markup = markup;
+    this.targetDate = targetDate;
+    this.intID = null;
+    this.deltaTime = 0;
+  }
+  start() {
+    this.intID = setInterval(() => {
+      this.deltaTime = this.targetDate - Date.now();
+      //console.log(this.targetDate);
+      const days = this.pad(Math.floor(this.deltaTime / (1000 * 60 * 60 * 24)));
+      const hours = this.pad(
+        Math.floor((this.deltaTime % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
+      );
+      const mins = this.pad(
+        Math.floor((this.deltaTime % (1000 * 60 * 60)) / (1000 * 60)),
+      );
+      const secs = this.pad(Math.floor((this.deltaTime % (1000 * 60)) / 1000));
+      this.insertValues(days, hours, mins, secs);
+    }, 1000);
+  }
+
+  // ===========
+  pad(value) {
+    return String(value).padStart(2, '0');
+  }
+  insertValues(d, h, m, s) {
+    const { daysTimer, hoursTimer, minsTimer, secsTimer } = this.markup;
+    daysTimer.textContent = d;
+    hoursTimer.textContent = h;
+    minsTimer.textContent = m;
+    secsTimer.textContent = s;
+  }
+  // ===========
+}
+
+const myTimer = new CountdownTimer(new Date('Jan 1, 2022'), {
+  selector: '#timer-1',
+  targetDate: new Date('Jan 1, 2022'),
+  daysTimer,
+  hoursTimer,
+  minsTimer,
+  secsTimer,
+});
+
+myTimer.start();
--- js/refs.js
+++ js/refs.js
@@ -1,6 +1,6 @@
 export default {
-  daysT: document.querySelector('[data-value="days"]'),
-  hoursT: document.querySelector('[data-value="hours"]'),
-  minsT: document.querySelector('[data-value="mins"]'),
-  secsT: document.querySelector('[data-value="secs"]'),
+  daysTimer: document.querySelector('[data-value="days"]'),
+  hoursTimer: document.querySelector('[data-value="hours"]'),
+  minsTimer: document.querySelector('[data-value="mins"]'),
+  secsTimer: document.querySelector('[data-value="secs"]'),
 };
